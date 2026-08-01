a0fa03d2dd1dd1a60fd11f1ff62dbf6ad1775dee
hosinoxfehmi <71970237+FahmiDeveloper@users.noreply.github.com>
2026-08-01 13:09:46 +0100

    part three

--- src/main/webapp/app/shared/services/pdf-export-patient_profile.service.ts
+++ src/main/webapp/app/shared/services/pdf-export-patient_profile.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { EmergencyContact, PatientProfile } from 'app/patient/profil/profile.component';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-import { Content, TDocumentDefinitions } from 'pdfmake/interfaces';
+import type { TDocumentDefinitions, Content } from 'pdfmake/interfaces';
 
 (pdfMake as any).vfs = (pdfFonts as any)['vfs'];
 
